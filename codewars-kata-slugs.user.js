e415022ec10745c7875a85af7e90b770fb4302fa
Joe Taylor <joe@textninja.net>
2021-11-27 12:33:17 -0800

    Updated so it works on more pages, with turbolinks. Added caching.

--- codewars-kata-slugs.user.js
+++ codewars-kata-slugs.user.js
@@ -9,45 +9,60 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+    let cache = {};
 
-    if (!/^\/kata\/[0-9a-f]+$/.test(location.pathname)) {
-        return;
-    }
+    document.addEventListener(
+        "turbolinks:load",
+        (function(f) { f(); return f; }(
+            function() {
+                'use strict';
 
-    let kataId = location.pathname.split("/").pop();
-    let kataTitleEl = document.querySelector("h4");
+                if (!/^\/kata\/[0-9a-f]+(\/|$)/.test(location.pathname)) {
+                    return;
+                }
 
-    // create flexbox wrapper for kata title + slug
-    let wrapper = document.createElement("div");
-    Object.assign(
-        wrapper.style,
-        {
-            display: "flex",
-            flexFlow: "column nowrap",
-            lineHeight: "0.8rem",
-            margin: "0.5rem 0 0.5rem 1rem"
-        }
+                let kataId = location.pathname.split("/")[2];
+                let kataTitleEl = document.querySelector("h4");
+
+                // create flexbox wrapper for kata title + slug
+                let wrapper = document.createElement("div");
+                Object.assign(
+                    wrapper.style,
+                    {
+                        display: "flex",
+                        flexFlow: "column nowrap",
+                        lineHeight: "0.8rem",
+                        margin: "0.5rem 0 0.5rem 1rem"
+                    }
+                );
+                kataTitleEl.insertAdjacentElement("beforeBegin", wrapper);
+
+                // wrap the kata title in a flexbox and update title CSS
+                wrapper.appendChild(kataTitleEl);
+                kataTitleEl.setAttribute("style", "margin: 0 !important;")
+
+                // add slug placeholder
+                let slugEl = document.createElement("div");
+                slugEl.innerHTML = "&nbsp;";
+                Object.assign(slugEl.style, { fontSize: "0.8rem", marginTop: "0", color: "var(--color-ui-text-lc)" });
+                wrapper.appendChild(slugEl);
+
+                // update slug
+                (async function() {
+                    if (cache[kataId]) {
+                        slugEl.textContent =cache[kataId];
+                        return;
+                    }
+
+                    let response = await fetch(`/api/v1/code-challenges/${kataId}`);
+                    let result = await response.json();
+
+                    let slug = result.slug;
+                    cache[kataId] = slug;
+                    slugEl.textContent = slug;
+
+                })();
+            }
+        ))
     );
-    kataTitleEl.insertAdjacentElement("beforeBegin", wrapper);
-
-    // wrap the kata title in a flexbox and update title CSS
-    wrapper.appendChild(kataTitleEl);
-    kataTitleEl.setAttribute("style", "margin: 0 !important;")
-
-    // add slug placeholder
-    let slugEl = document.createElement("div");
-    slugEl.innerHTML = "&nbsp;";
-    Object.assign(slugEl.style, { fontSize: "0.8rem", marginTop: "0", color: "var(--color-ui-text-lc)" });
-    wrapper.appendChild(slugEl);
-
-    // update slug
-    (async function() {
-        let response = await fetch(`/api/v1/code-challenges/${kataId}`);
-        let result = await response.json();
-
-        let slug = result.slug;
-        slugEl.textContent = slug;
-    })();
-
 })();
