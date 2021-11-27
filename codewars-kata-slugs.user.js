0cfdae097f9eb64270e28794103b724888f5c165
Joe Taylor <joe@textninja.net>
2021-11-27 11:28:44 -0800

    changed namespace url

--- codewars-kata-slugs.user.js
+++ codewars-kata-slugs.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Codewars Kata Slugs
-// @namespace    https://textninja.net/
+// @namespace    https://www.textninja.net
 // @version      1.0-dev.1
 // @description  Show the slug for codewars challenges
 // @author       Joe Taylor
