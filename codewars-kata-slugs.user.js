4ce4bebf80dfc5bb19a3d37586dd98d99ee035d6
Joe Taylor <joe@textninja.net>
2021-11-27 11:27:14 -0800

    Hello world for a start.

---
+++ codewars-kata-slugs.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Codewars Kata Slugs
+// @namespace    https://textninja.net/
+// @version      1.0-dev.1
+// @description  Show the slug for codewars challenges
+// @author       Joe Taylor
+// @match        http://codewars.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("Hello");
+})();
